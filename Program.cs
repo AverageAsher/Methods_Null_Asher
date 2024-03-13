91aad1e7b3a961e23b936755f11103a87c73d7c9
Someidiotter <anullowo@gmail.com>
2024-03-13 14:25:28 -0500

    Done 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Multiplication method
+            int result = Multiply(5, 3);
+            Console.WriteLine("Multiplication result: " + result);
+
+            // Welcome method
+            WelcomeUser();
+
+            // Ask for user's name
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            // Greet user
+            GreetUser(userName);
+
+            // Farewell method
+            Farewell();
+        }
+
+        static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        static void WelcomeUser()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static void GreetUser(string name)
+        {
+            Console.WriteLine("Hello, " + name + "! It is nice to meet you!");
+        }
+
+        static void Farewell()
+        {
+            Console.WriteLine("Have a fantastic day!");
         }
     }
 }
